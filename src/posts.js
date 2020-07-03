fce119e8c9a5f1c8d07281bebcaf5e6c2f4cc50e
Joshua B Gutierrez <joshgm15@gmail.com>
2020-07-02 22:06:20 -0500

    Blog data

---
+++ src/posts.js
@@ -0,0 +1,31 @@
+import img1 from './images/1.jpg';
+import img2 from './images/2.jpg';
+import img3 from './images/3.jpg';
+import img4 from './images/4.jpg';
+
+export const posts = [
+	{
+		id: 0,
+		tag: 'Report',
+		title: 'Explore Instagram Video: How to convert, engage, and get more Instagra...',
+		postImg: img1
+	},
+	{
+		id: 1,
+		tag: 'Blog Post',
+		title: 'How to make social video work for your marketing team',
+		postImg: img2
+	},
+	{
+		id: 2,
+		tag: 'Webinar',
+		title: 'The Video Forecast: 2020 predictions from Industry leaders',
+		postImg: img3
+	},
+	{
+		id: 3,
+		tag: 'Report',
+		title: 'Winning strategies for digital video',
+		postImg: img1
+	}
+];
