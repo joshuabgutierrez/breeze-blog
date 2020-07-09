f0bf8692846f44630841562afdbbb71a673aa340
Joshua B Gutierrez <joshgm15@gmail.com>
2020-07-09 18:44:14 -0500

    Home Component

---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from '../components/Main';
+import NewPosts from '../components/NewPosts';
+
+export default function Home() {
+	return (
+		<div>
+			<Main />
+			<NewPosts />
+		</div>
+	);
+}
